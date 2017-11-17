991b189d68ac084185f2196e077029a568c74f19
Sinkemlow <jamesalanthompson@gmail.com>
2017-11-17 15:21:59 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@
 class users (
   $admins_group_id        = hiera('users_admins_group_id',           $users::params::admins_group_id),
   $admins_group_name      = hiera('users_admins_group_name',         $users::params::admins_group_name),
-  $admins_ssh_keys        = hiera_hash('users::admins_ssh_keys',    $users::params::admins_ssh_keys),
-  $admins_users           = hiera_hash('users::admins_users',       $users::params::admins_users),
+  $admins_ssh_keys        = hiera_hash('users::admins_ssh_keys',     $users::params::admins_ssh_keys),
+  $admins_users           = hiera_hash('users::admins_users',        $users::params::admins_users),
 
 ) inherits users::params {
 
