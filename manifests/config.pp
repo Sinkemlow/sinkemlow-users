fda57eb2d2e511fda65c6eb4483ec2fe832b56fe
Sinkemlow <jamesalanthompson@gmail.com>
2017-11-21 09:22:42 +0000

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -21,23 +21,23 @@ class users::config {
 
   create_resources('ssh_authorized_key', $users::admins_ssh_keys, $key_defaults)
   
-  group { $users::zonza_projects_group_name:
+  group { $users::app_group_name:
     ensure => 'present',
-    gid    => $users::zonza_projects_group_id,
+    gid    => $users::app_group_id,
   }
 
-  user { $users::zonza_projects_user_name:
+  user { $users::app_user_name:
     ensure     => 'present',
-    uid        => $users::zonza_projects_user_id,
-    gid        => $users::zonza_projects_group_name,
+    uid        => $users::app_user_id,
+    gid        => $users::app_group_name,
     managehome => true,
   }
   
-  file { '/var/log/hogarth/':
+  file { '/var/log/app/':
     ensure  => 'directory',
     recurse => true,
-    owner   => $users::zonza_projects_user_name,
-    require => User[$users::zonza_projects_user_name],
+    owner   => $users::app_user_name,
+    require => User[$users::app_user_name],
   }
 }
 
