7947d9fd71a904725734bc407f6bab641a77bee8
Sinkemlow <jamesalanthompson@gmail.com>
2017-11-21 09:17:23 +0000

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,5 +20,24 @@ class users::config {
   }
 
   create_resources('ssh_authorized_key', $users::admins_ssh_keys, $key_defaults)
+  
+  group { $users::zonza_projects_group_name:
+    ensure => 'present',
+    gid    => $users::zonza_projects_group_id,
+  }
+
+  user { $users::zonza_projects_user_name:
+    ensure     => 'present',
+    uid        => $users::zonza_projects_user_id,
+    gid        => $users::zonza_projects_group_name,
+    managehome => true,
+  }
+  
+  file { '/var/log/hogarth/':
+    ensure  => 'directory',
+    recurse => true,
+    owner   => $users::zonza_projects_user_name,
+    require => User[$users::zonza_projects_user_name],
+  }
 }
 
